6e2c3f1df3db8af98f7c6e2ad4c9ae0d9485eeec
Casper <joeharris461767@gmail.com>
2020-01-31 16:44:53 +0000

    Add quickcord message

--- src/embed.js
+++ src/embed.js
@@ -1,5 +1,12 @@
 const discord = require('discord.js');
+const package = require('../package.json');
 
 module.exports = props => {
-    return new discord.RichEmbed(props);
+    const embed = new discord.RichEmbed(props);
+
+    const footer = embed.footer || `- Made with Quickcord v${package.version}`;
+
+    embed.setFooter(footer);
+
+    return embed;
 }
