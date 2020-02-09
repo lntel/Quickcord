a5f576986f15f14071e4e21d00205d0002486206
Casper <joeharris461767@gmail.com>
2020-02-09 12:25:00 +0000

    Add method comments

--- src/api-handler.js
+++ src/api-handler.js
@@ -37,6 +37,13 @@ class Api {
     //     }
     // }
 
+    /**
+     * The POST request endpoint
+     * @param {string} endpoint The api endpoint
+     * @param {object} data The json POST body
+     * @param {function} callback Callback method
+     * @param {object} headers Optional additional headers
+     */
     post(endpoint, data, callback, headers = {}) {
         fetch(`${this.api}/${endpoint}`, {
             method: 'POST',
@@ -52,6 +59,13 @@ class Api {
         .catch(console.error);
     }
 
+    /**
+     * The PUT request method
+     * @param {string} endpoint Api endpoint
+     * @param {object} data PUT json body
+     * @param {function} callback Callback function
+     * @param {object} headers Optional headers
+     */
     put(endpoint, data, callback, headers = {}) {
         fetch(`${this.api}/${endpoint}`, {
             method: 'PUT',
@@ -67,6 +81,13 @@ class Api {
         .catch(console.error);
     }
 
+    /**
+     * The PATCH request method
+     * @param {string} endpoint Api endpoint
+     * @param {object} data PATCH json body
+     * @param {function} callback Callback function
+     * @param {object} headers Optional headers
+     */
     patch(endpoint, data, callback, headers = {}) {
         fetch(`${this.api}/${endpoint}`, {
             method: 'PATCH',
