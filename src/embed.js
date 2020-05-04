67d4a9c43c819c001d3020f4e01102975c35078d
Casper <joeharris461767@gmail.com>
2020-05-04 18:14:25 +0100

    Fixed API breaking change

--- src/embed.js
+++ src/embed.js
@@ -2,7 +2,7 @@ const discord = require('discord.js');
 const package = require('../package.json');
 
 module.exports = props => {
-    const embed = new discord.RichEmbed(props);
+    const embed = new discord.MessageEmbed(props);
 
     const footer = embed.footer || `- Quickcord v${package.version}`;
 
