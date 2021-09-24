65bc4710a62635489cb505a1aedd3752837bbfcf
lntel <joeharris461767@gmail.com>
2021-09-24 23:18:03 +0100

    Removed API stuff

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,11 @@
 import { MessageEmbed as RichEmbed } from 'discord.js';
 import Client, { CommandCallback, CommandOptions, CommandParameters, LoadedCommand } from './client';
-import Api from './request';
 import Question from './utilities/question'
 import ReactionHandler from './utilities/reaction'
 import EmbedPaginator from './embed/paginator';
 
 export {
     Client,
-    Api,
     RichEmbed,
     EmbedPaginator,
     CommandCallback,
